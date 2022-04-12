ef72b1ab3225dc88631da5fccd99242f159c9eda
VriskaSerket51 <77536295+VriskaSerket51@users.noreply.github.com>
2022-04-12 20:55:24 +0900

    Create sequential_search.py

---
+++ sequential_search.py
@@ -0,0 +1,17 @@
+import random
+
+idx = 0
+array = list(range(100000))
+random.shuffle(array)
+
+idx = 0
+key = int(input())
+
+for i in array:
+    if i == key:
+        print("%d found at %d" % (key, idx))
+        break
+    idx += 1
+
+if idx == len(array):
+    print("404 Not Found")
